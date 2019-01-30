00d83b28e7d7ba3b13bc63db778d97f3444e8c46
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2019-01-30 21:57:18 +0800

    Rebuild logo spinner (Commit SHA-1 a7cccb1e)

--- main.php
+++ main.php
@@ -11,9 +11,9 @@ function add_logo_spinner_1() {
     $dir = plugin_dir_url( __FILE__ );
 
     return "<logo-spinner-1></logo-spinner-1>" .
-        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/runtime.4ccf67c919f9077afc3e.js\"></script>" .
-        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/polyfills.c6871e56cb80756a5498.js\"></script>" .
-        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/main.6f7a2507a851aa64b647.js\"></script>";
+        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/runtime.95ba26ba764e20834375.js\"></script>" .
+        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/polyfills.b79c9d41d08722089ce4.js\"></script>" .
+        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/main.d68e4757c37bb71d632f.js\"></script>";
 }
 
 add_shortcode('logo_spinner_1', 'add_logo_spinner_1');
