e0ac6e365ff9542acb7b9db4303a429d73f1a098
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2019-05-17 02:27:41 +0800

    Rebuild (Commit SHA-1: a7cccb1e)

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name: SVG Playground
 * Description: Include a few SVG-related demos with shortcode [logo_spinner_1]
-* Version: 1.0
+* Version: 1.0.1
 * Author: Yi Yang
 * Author URI: https://blog.ahyangyi.org
 */
@@ -11,9 +11,9 @@ function add_logo_spinner_1() {
     $dir = plugin_dir_url( __FILE__ );
 
     return "<logo-spinner-1></logo-spinner-1>" .
-        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/runtime.95ba26ba764e20834375.js\"></script>" .
-        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/polyfills.b79c9d41d08722089ce4.js\"></script>" .
-        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/main.d68e4757c37bb71d632f.js\"></script>";
+        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/runtime.58db6607081ba1289d07.js\"></script>" .
+        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/polyfills.7e61eeabe4f75fabb7d3.js\"></script>" .
+        "<script type=\"text/javascript\" src=\"" . $dir . "/logo_spinner_1/main.f4b4f5f8ddc9dc375556.js\"></script>";
 }
 
 add_shortcode('logo_spinner_1', 'add_logo_spinner_1');
